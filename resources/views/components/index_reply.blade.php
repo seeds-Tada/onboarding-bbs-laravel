f9c6bdf919045bb8554324e22ea8f789ab4defea
seeds-Tada <k.tada@seeds-std.co.jp>
2025-11-14 15:13:02 +0900

    1114_返信を削除した時に未定義ルートにPOSTしてエラーになる問題を修正しました。

--- resources/views/components/index_reply.blade.php
+++ resources/views/components/index_reply.blade.php
@@ -20,7 +20,7 @@
 							<button type="submit">編集</button>
 						</form>
 						&nbsp;
-						<form action="{{ url('/delete_confirm/'.$article['id']) }}" method="post">
+						<form action="{{ url('/delete_complete/'.$article['id']) }}" method="post">
 							@csrf
 							<input type="hidden" name="id" value="{{ $article['id'] }}">
 							<button type="submit">削除</button>
